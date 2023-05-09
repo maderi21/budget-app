eff428a64808ee24a0d599c417610e75d563ca77
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-09 12:28:13 +0200

    react components

---
+++ budget/src/App.js
@@ -0,0 +1,26 @@
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,mimax(300px,1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard></BudgetCard>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
